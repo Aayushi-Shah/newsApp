c2e8fd3e93221ee491097d2a2979bf2ddb6f01dc
Aayushi Shah <aayushi@Aayushis-MacBook-Pro.local>
2020-02-27 18:10:55 +0530

    Added fallback for networkFirst fn

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ window.addEventListener('load', async e => {
     await updateSources();
     sourceSelector.value = defaultSource;
 
-    sourceSelector.addEventListener('change', e=>{
+    sourceSelector.addEventListener('change', e =>{
         updateNews(e.target.value)
     })
 
--- sw.js
+++ sw.js
@@ -1,4 +1,7 @@
+// Add CDN
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
+
+// Define staticAssets so they can we cached, and you do not need to make network call each time
 const staticAssets = [
     './',
     './styles.css',
@@ -7,8 +10,28 @@ const staticAssets = [
     './images/fetch-dog.jpeg'
 ]
 
+// Cache staticAssets
 workbox.precaching.precacheAndRoute(staticAssets)
-workbox.routing.registerRoute(new RegExp('https://newsapi.org/(.*)'), new workbox.strategies.NetworkFirst())
+
+// create a NetworkFirst instance
+var networkFirst = new workbox.strategies.NetworkFirst({
+    cacheName: 'cache-pages' 
+  });
+  
+// check whether network call returns a response, if it fails render fallback.json
+const customHandler = async (args) => {
+    try {
+        const response = await networkFirst.handle(args);
+        return response || await caches.match('./fallback.json');
+    } catch (error) {
+        return await caches.match('./fallback.json');
+    }
+}; 
+
+// call networkFirst function for the request
+workbox.routing.registerRoute(new RegExp('https://newsapi.org/(.*)'), customHandler)
+
+// render images from cache, if not available then call network
 workbox.routing.registerRoute(/.*\.(png|jpg|jpeg|gif)/, new workbox.strategies.CacheFirst({
     cacheName: 'new-images',
     cacheExpiration : {maxEntries: 20, maxAgeseconds: 12 * 60 * 60},
